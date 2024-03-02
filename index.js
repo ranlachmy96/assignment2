3f713c0c172261578b4f9b653deb76c1af4a5468
Ran Lachmy <ranlachmy96@gmail.com>
2024-03-02 22:09:49 +0200

    move cores

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 // const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
-app.use(cors());
+
 
 const {
   familyReunificationRouter,
