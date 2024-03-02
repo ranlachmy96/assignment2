648e7fbf89f5d29e639af00052d9fc1a8cc28f65
Ran Lachmy <ranlachmy96@gmail.com>
2024-03-02 22:15:38 +0200

    added the server to the index

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-/** ***************************************************
- * Express Application Setup
- * - Loads environment variables
- * - Handles asynchronous errors
- * - Sets up middleware for parsing requests and custom logging
- * - Defines routes and mounts routers
- * - Implements global error handling
- * - Starts the Express server and listens on the specified port
- **************************************************** */
 require('dotenv').config();
 require('express-async-errors');
 const cors = require('cors');
 const express = require('express');
 const logger = require('./loggers/logger');
 const { errorHandler } = require('./middlewares/errorHandler');
+const {
+  familyReunificationRouter,
+} = require('./routers/familyReunification.router');
 
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
-
-const {
-  familyReunificationRouter,
-} = require('./routers/familyReunification.router');
-
 app.use('/familyReunification', familyReunificationRouter);
 
-// app.listen(port, () => {
-//   console.log(`Express server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Express server is running on port ${port}`);
+});
 
 module.exports = app;
--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./index');
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Express server is running on port ${port}`);
-});
