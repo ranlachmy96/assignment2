2ada117a2406d2fc10437f07327a2e5a5d8e1b19
Ran Lachmy <ranlachmy96@gmail.com>
2024-03-02 22:32:16 +0200

    allow from anywhere

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const {
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
